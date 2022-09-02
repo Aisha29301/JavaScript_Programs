22bb17c9ad6854e90dfa2abae7c302a3363b25c4
Aishwarya Lokhande <111597400+Aisha29301@users.noreply.github.com>
2022-09-02 23:06:11 +0530

    Delete Q1.js

--- Q1.js
+++
@@ -1,8 +0,0 @@
-// Write a program to find whether a given year is a leap year or not.
-
-let year = 2001;
-// year = 2004;
-if(((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
-    console.log(year+" is a leap year");  
-else
-    console.log(year+" is not a leap year");  
